64084721226c20a554069b00402ecc5e44eb7454
NadeenAhmed420 <nadeenahmed420@gmail.com>
2024-01-16 04:29:48 +0200

    LAST EDIT

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ items.forEach((item) => {
     target.classList.add("active");
   });
 });
+
 //////////////////////////////////////////////////
 const pop = document.querySelector("#pop");
 function openPop() {
@@ -57,106 +58,6 @@ dropDowns.forEach((dropDown) => {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //ANIMATION
-
-const observerR = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log("entry");
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show-R");
-    }
-  });
-});
-const hiddenElementR = document.querySelectorAll(".hidden-R");
-hiddenElementR.forEach((el) => observerR.observe(el));
-
-/////////////////////////////////////////////////////////////////////
-
-const observerL = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log("entry");
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show-L");
-    }
-  });
-});
-const hiddenElementL = document.querySelectorAll(".hidden-L");
-hiddenElementL.forEach((el) => observerL.observe(el));
-
-/////////////////////////////////////////////////////////////////////
-
-const observerC = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log("entry");
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show-C");
-    }
-  });
-});
-const hiddenElementC = document.querySelectorAll(".hidden-C");
-hiddenElementC.forEach((el) => observerC.observe(el));
-
-////////////////////////////////////////////////////
-
-const observerOPA = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log("entry");
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show-OPA");
-    }
-  });
-});
-const hiddenElementOPA = document.querySelectorAll(".hidden-OPA");
-hiddenElementOPA.forEach((el) => observerOPA.observe(el));
-
-////////////////////////////////////////////////
-
-const observerRO = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log("entry");
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show-RO");
-    }
-  });
-});
-const hiddenElementRO = document.querySelectorAll(".hidden-RO");
-hiddenElementRO.forEach((el) => observerRO.observe(el));
-//////////////////////////////////////////////////////
-
-/////////////////////////////////////////
-//CONTROL-CATEGORY
-
-document.addEventListener("DOMContentLoaded", function () {
-  const sliderInner = document.querySelector(".slider-inner");
-  const sliderInner2 = document.querySelector(".slider-inner2");
-  const scrollSpeed = 10; // Adjust the scroll speed as needed
-  let scrollPosition = 0;
-
-  function scrollUp() {
-    // Incrementally move the slider upwards
-    scrollPosition -= scrollSpeed;
-    sliderInner.style.transform = `translateY(${scrollPosition}px)`;
-    sliderInner2.style.transform = `translateY(${scrollPosition}px)`;
-
-    // Check if it has reached the end, then reset position
-    if (scrollPosition <= -sliderInner.scrollHeight) {
-      scrollPosition = 0;
-      sliderInner.style.transition = "none"; // Disable transition during the reset
-      sliderInner.style.transform = `translateY(${scrollPosition}px)`;
-      sliderInner2.style.transition = "none"; // Disable transition during the reset
-      sliderInner2.style.transform = `translateY(${scrollPosition}px)`;
-
-      // Enable transition after the reset
-      setTimeout(() => {
-        sliderInner.style.transition = "";
-        sliderInner2.style.transition = "";
-      }, 0);
-    }
-  }
-
-  // Set the interval for scrolling
-  setInterval(scrollUp, 50); // Scroll every 50 milliseconds
-});
-
 
 
 wow = new WOW(
